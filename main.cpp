9e0a35a12e085093ce1f15d07c3c3d2da5dddd47
Alehmann04 <la59021@mail.harpercollege.edu>
2024-10-16 16:47:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <exception>
 using namespace std;
 
-int pickyFunction(int &a, int &b, int &c){
+class exception3: public exception {
+  virtual const char* what() const throw() {
+    return "My exception happened";
+  }
+} ex1;
 
-    return (a+b+c);
+class exception2: public exception {
+  virtual const char* what() const throw() {
+    return "My exception happened";
+  }
+} ex2;
+
+class exception3: public exception {
+  virtual const char* what() const throw() {
+    return "My exception happened";
+  }
+} ex3;
+
+int what(int a, int b, int c) {
+    try {
+        if 
+    }
+    catch(){
+    }
+    return a + b + c;
 }
 
-int main()
-{
+int main() {
 
 int a = 0;
 int b = 0;
@@ -22,9 +44,7 @@ cin >> b;
 cout << "Enter an Integer: ";
 cin >> c;
 
-pickyFunction(a, b, c);
+what(a, b, c);
 
-return;
-
-
+return 0;
 }
