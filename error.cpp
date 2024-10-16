83976df0175cb9b06abf84c97dd12d2f37006284
Alehmann04 <alehmann@post.com>
2024-10-16 16:45:09 -0500

    Create error.cpp

---
+++ error.cpp
@@ -0,0 +1,4 @@
+// If argument one is greater than zero, throw a custom "argumentoneisnogood" exception.
+// If argument two is equal to argument one, throw a custom "theycantbethesame" exception.
+// If argument three is equal to the sum of argument one and argument two, throw a custom "theycantadd" exception.
+// Write a main function that handles all of these exceptions and prints out their error messages.
